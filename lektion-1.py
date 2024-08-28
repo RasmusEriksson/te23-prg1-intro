34571afe0cc64a9a0f9816fa260b6ebbdcce7e15
RasmusEriksson <Rasmus.Eriksson@elev.ga.ntig.se>
2024-08-28 09:29:46 +0200

    did some variable stuff yeah

---
+++ lektion-1.py
@@ -0,0 +1,9 @@
+Name = input("Write yo name")
+
+print(Name)
+
+if Name == "Rasmus":
+    print(f"wow your name is {Name}, that sounds really cool!")
+else:
+     print(f"wow your name is {Name}, thats ok")
+    
