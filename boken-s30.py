00f25585bc07ee5945da0dbe96c6002f13f7b5fa
RasmusEriksson <Rasmus.Eriksson@elev.ga.ntig.se>
2024-09-04 09:27:51 +0200

    I fixed my code, WOOO!!!

---
+++ boken-s30.py
@@ -0,0 +1,4 @@
+svar = input("Skriv ett tal ")
+x = float(svar)
+y = x * x
+print(f"{svar} i kvadrat är: {y:04.2f}")
--- lektion-2.py
+++ lektion-2.py
@@ -1,8 +1,8 @@
 print("Yuh wassup, this my program")
 # Print a welcoming statment
-Name = input("What's yo name:  ")
+name = input("\tWhat's yo name:  ")
 # Let the user assign a variable via the input function
-print(f"Oh, welcome {Name}!")
+print(f"Oh, welcome {name}!")
 # Formating both string and the variable with f
 # Yeah not writing comments everywhere haha :P'ä´
 
@@ -12,17 +12,16 @@ print("Now my next question will be, how old are you?")
 
 
 def DefineAge():
-        Age = input("Input Your Age: ")
-        if Age:
-            if Age.isdigit() == True:
-                print(Age)
-                return int(Age)
-            else:
-                print("Sorry, please input a valid number for age!")
-                DefineAge()
-        else:
-            print("Please write a value in the input!")
-            DefineAge()
+        
+        while True:
+             age = input("\tInput your age: ")
+             if age:
+                  if age.isdigit() == True:
+                       return(int(age))
+                  else:
+                       print("Sorry! Please input a valid number for your age! (like \"16\" or \"72\")")
+             else:
+                  print("Sorry! Please input a value! (like \"16\" or \"72\")")
         
             
         
@@ -34,18 +33,18 @@ def DefineAge():
 
 # Doesn't work, not my problem
 
-UserAge = DefineAge()  
+userAge = DefineAge()  
 
 
     
-if UserAge >= 15:
-    print(f"Wow, you are a ripe age of {UserAge} years old!")
+if userAge >= 15:
+    print(f"Wow, you are a ripe age of {userAge} years old!")
 else:
-    print(f"You're only {UserAge}? that's way too young")
+    print(f"You're only {userAge}? that's way too young")
 
     
 
-print(f"That means that you will be {UserAge + 1} years old next year!")
+print(f"That means that you will be {userAge + 1} years old next year!")
 
 
 
